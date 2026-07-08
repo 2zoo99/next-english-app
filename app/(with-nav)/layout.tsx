4f727520fcd2d5673c37933fb5852d7aca00f858
2zoo99 <20234120@gm.hannam.ac.kr>
2026-07-08 16:11:59 +0900

    change layout.tsx

--- app/(with-nav)/layout.tsx
+++ app/(with-nav)/layout.tsx
@@ -1,4 +1,6 @@
 import Layout from "@/utils/components/layout/Layout"
+//with-nav는 navigation bar가 있는 레이아웃을 의미합니다.
+//web에서 navigation bar가 있는 레이아웃을 사용하고 싶을 때, 이 레이아웃을 사용하면 됩니다.
 
 export default function WithNavLayout({ children }: { children: React.ReactNode }) {
     return (
