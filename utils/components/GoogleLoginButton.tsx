249a236014a10979285fda8a8621a7b52d0caa10
2zoo99 <20234120@gm.hannam.ac.kr>
2026-08-30 09:47:41 +0900

    google login component updated

--- utils/components/GoogleLoginButton.tsx
+++ utils/components/GoogleLoginButton.tsx
@@ -9,6 +9,9 @@ export default function GoogleLoginButton() {
             provider: 'google',
             options: {
                 redirectTo: `${window.location.origin}/auth/callback`,
+                queryParams: {
+                    prompt: 'select_account',   // 추가
+                },
             },
         });
     }
